d2fabd447b752737abd9d41940e1195dc8be3ab0
Makayla-Law <100392989+Makayla-Law@users.noreply.github.com>
2023-07-01 22:12:48 -0500

    Update functions.py

--- functions.py
+++ functions.py
@@ -1,7 +1,7 @@
 num_x = 0.3529
 num_y = 0.051
 
-def hyp2(num1, num2, sf1, sf2):
+def hyp(num1, num2, sf1, sf2):
   sigfigs1 = num1*sf1
   sigfigs2 = num2*sf2
   sq = (sigfigs1**2)+(sigfigs2**2)
@@ -11,4 +11,4 @@ def hyp2(num1, num2, sf1, sf2):
 
 num_x = 0.3529
 num_y = 0.051
-hyp2(num_x, num_y,(10**(-5)), (10**(-5)))
+hyp(num_x, num_y,(10**(-5)), (10**(-5))) ##should return 3.56566136923e-06
